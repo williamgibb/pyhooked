e55f1d17843c9373619913f00f104184c673d216
William Gibb <williamgibb@gmail.com>
2016-08-14 23:04:11 -0400

    Move low level handlers to self instead of defining them in a class function.

--- pyhooked/__init__.py
+++ pyhooked/__init__.py
@@ -71,6 +71,46 @@ class Hook(object):
         self.mouse_is_hook = False
         self.keyboard_is_hook = True
 
+    def keyboard_low_level_handler(self, code, event_code, kb_data_ptr):
+        """Used to catch keyboard events and deal with the event"""
+        try:
+            key_code = 0xFFFFFFFF & kb_data_ptr[0]  # key code
+            current_key = constants.ID_TO_KEY[key_code]
+            event_type = constants.event_types[0xFFFFFFFF & event_code]
+
+            if event_type == 'key down':  # add key to those down to list
+                self.pressed_keys.append(current_key)
+
+            if event_type == 'key up':  # remove when no longer pressed
+                self.pressed_keys.remove(current_key)
+
+            # wrap the keyboard information grabbed into a container class
+            event = KeyboardEvent(current_key, event_type, self.pressed_keys, key_code)
+
+            # if we have an event handler, call it to deal with keys in the list
+            if self.handler:
+                self.handler(event)
+
+        finally:
+            # TODO: fix return here to use non-blocking call
+            return CallNextHookEx(self.keyboard_id, code, event_code, kb_data_ptr)
+
+    def mouse_low_level_handler(self, code, event_code, kb_data_ptr):
+        """Used to catch and deal with mouse events"""
+        try:
+            current_key = constants.MOUSE_ID_TO_KEY[event_code]
+            if current_key != 'Move':  # if we aren't moving, then we deal with a mouse click
+                event_type = constants.MOUSE_ID_TO_EVENT_TYPE[event_code]
+                # the first two members of kb_data_ptr hold the mouse position, x and y
+                event = MouseEvent(current_key, event_type, kb_data_ptr[0], kb_data_ptr[1])
+
+                if self.handler:
+                    self.handler(event)
+
+        finally:
+            # TODO: fix return here to use non-blocking call
+            return CallNextHookEx(self.mouse_id, code, event_code, kb_data_ptr)
+
     def hook(self, keyboard=True, mouse=False):
         """Hook mouse and/or keyboard events"""
         self.mouse_is_hook = mouse
@@ -81,55 +121,14 @@ class Hook(object):
             raise Exception("You must hook into either the keyboard and/or mouse events")
 
         if self.keyboard_is_hook:
-            def keyboard_low_level_handler(code, event_code, kb_data_ptr):
-                """Used to catch keyboard events and deal with the event"""
-                try:
-                    key_code = 0xFFFFFFFF & kb_data_ptr[0]  # key code
-                    current_key = constants.ID_TO_KEY[key_code]
-                    event_type = constants.event_types[0xFFFFFFFF & event_code]
-
-                    if event_type == 'key down':  # add key to those down to list
-                        self.pressed_keys.append(current_key)
-
-                    if event_type == 'key up':  # remove when no longer pressed
-                        self.pressed_keys.remove(current_key)
-
-                    # wrap the keyboard information grabbed into a container class
-                    event = KeyboardEvent(current_key, event_type, self.pressed_keys, key_code)
-
-                    # if we have an event handler, call it to deal with keys in the list
-                    if self.handler:
-                        self.handler(event)
-
-                finally:
-                    # TODO: fix return here to use non-blocking call
-                    return CallNextHookEx(self.keyboard_id, code, event_code, kb_data_ptr)
-
-            keyboard_pointer = _callback_pointer(keyboard_low_level_handler)
-
+            keyboard_pointer = _callback_pointer(self.keyboard_low_level_handler)
             self.keyboard_id = SetWindowsHookExA(constants.WH_KEYBOARD_LL,
                                                  keyboard_pointer,
                                                  GetModuleHandleA(None),
                                                  0)
 
         if self.mouse_is_hook:
-            def mouse_low_level_handler(code, event_code, kb_data_ptr):
-                """Used to catch and deal with mouse events"""
-                try:
-                    current_key = constants.MOUSE_ID_TO_KEY[event_code]
-                    if current_key != 'Move':  # if we aren't moving, then we deal with a mouse click
-                        event_type = constants.MOUSE_ID_TO_EVENT_TYPE[event_code]
-                        # the first two members of kb_data_ptr hold the mouse position, x and y
-                        event = MouseEvent(current_key, event_type, kb_data_ptr[0], kb_data_ptr[1])
-
-                        if self.handler:
-                            self.handler(event)
-
-                finally:
-                    # TODO: fix return here to use non-blocking call
-                    return CallNextHookEx(self.mouse_id, code, event_code, kb_data_ptr)
-
-            mouse_pointer = _callback_pointer(mouse_low_level_handler)
+            mouse_pointer = _callback_pointer(self.mouse_low_level_handler)
             self.mouse_id = SetWindowsHookExA(constants.WH_MOUSE_LL,
                                               mouse_pointer,
                                               GetModuleHandleA(None),
